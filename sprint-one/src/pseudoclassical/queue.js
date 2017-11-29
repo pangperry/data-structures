734e444b571a372da756a64ee1a8769454317006
Hack Reactor Student <student@hackreactor.com>
2017-11-29 15:57:29 -0800

    Add proto queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,20 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.head = 0;
+  this.tail = 0;
 };
 
-
+Queue.prototype.size = function() {
+  if (this.head > this.tail) {
+    this.head = this.tail;
+  }
+  return this.tail - this.head;
+};
+Queue.prototype.enqueue = function(value) {
+  this.tail++;  
+  this.storage[this.tail] = value;
+};
+Queue.prototype.dequeue = function() {
+  this.head++;
+  return this.storage[this.head]; 
+}; 
