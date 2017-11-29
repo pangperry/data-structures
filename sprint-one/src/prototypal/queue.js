42bd457df6cf50e7fde45d19c12b9ecc47ad2730
Hack Reactor Student <student@hackreactor.com>
2017-11-29 15:56:55 -0800

    Add prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
+  var someInstance = Object.create(queueMethods);
+  someInstance.head = 0;
+  someInstance.tail = 0;
+  someInstance.storage = {};
+  return someInstance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var queueMethods = {};
 
+queueMethods.size = function() {
+  if (this.head > this.tail) {
+    this.head = this.tail;
+  }
+  return this.tail - this.head;
+};
 
+queueMethods.enqueue = function(value) {
+  this.tail++;
+  this.storage[this.tail] = value;  
+};
+
+queueMethods.dequeue = function() {
+  this.head++;
+  return this.storage[this.head];
+};
