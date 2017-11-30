32e4248e32670788c8b1779824a2fa51d9583c87
Mark Perry <mark.j.perry11@gmail.com>
2017-11-29 20:05:49 -0800

    Fix linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,62 +1,48 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  
-  list.addToTail = function(value) {
+
+  list.addToTail = function (value) {
     var node = Node(value);
     if (list.tail !== null) {
-      node.next = list.tail;
+      list.tail.next = node;
+      list.tail = node;
     }
     list.tail = node;
     if (list.head === null) {
       list.head = node;
     }
-   //creates new node, assigns node value
-   //sets new node next value to the node reference by list head
-   //set list head to the new node  
   };
 
-  list.removeHead = function() {
-    var start, former;
-
-    if (list.tail.next === null) {
-      list.tail = null;
-      list.head = null;
-    } else {
-      start = list.tail;
-      while (start.next.next !== null) {
-        start = start.next;
-      }
-      former = start.next;
-      list.head = start;
-      list.head.next = null;
-    }
-    return former;
+  list.removeHead = function () {
+    var val = list.head.value;
+    list.head = list.head.next;
+    return val;
   };
 
 
-  list.contains = function(target) {
-    var start = list.tail;
+  list.contains = function (target) {
+    var start = list.head;
+
     if (start.value === target) {
-      return true; 
+      return true;
     }
-    
-    while (start.next.next !== null) {
+
+    while (start.next !== null) {
       start = start.next;
       if (start.value === target) {
         return true;
       }
     }
-    
     return false;
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
@@ -64,9 +50,3 @@ var Node = function(value) {
 
   return node;
 };
-var list = LinkedList();
-
-list.addToTail(5)
-/*
- * Complexity: What is the time complexity of the above functions?
- */
