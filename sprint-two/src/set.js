6ae7d0e6f12a72b47ce5483fa377a3c94c06a099
Mark Perry <mark.j.perry11@gmail.com>
2017-11-30 11:29:03 -0800

    Add set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  set._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return !!set._storage[item];
 };
 
 setPrototype.remove = function(item) {
+  delete set._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+var set = Set();
+set.add(9);
+set.add(10);
+set.contains(8);
+set.remove(9);
