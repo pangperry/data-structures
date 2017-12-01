9588e53b5170f5f3dee01bea59decfd21f2b447e
Mark Perry <mark.j.perry11@gmail.com>
2017-12-01 13:19:19 -0800

    Copy testing setup

---
+++ advanced/src/doubleLinkedList.js
@@ -0,0 +1,52 @@
+var DoubleLinkedList = function () {
+//   var list = {};
+//   list.head = null;
+//   list.tail = null;
+
+
+//   list.addToTail = function (value) {
+//     var node = Node(value);
+//     if (list.tail !== null) {
+//       list.tail.next = node;
+//       list.tail = node;
+//     }
+//     list.tail = node;
+//     if (list.head === null) {
+//       list.head = node;
+//     }
+//   };
+
+//   list.removeHead = function () {
+//     var val = list.head.value;
+//     list.head = list.head.next;
+//     return val;
+//   };
+
+
+//   list.contains = function (target) {
+//     var start = list.head;
+
+//     if (start.value === target) {
+//       return true;
+//     }
+
+//     while (start.next !== null) {
+//       start = start.next;
+//       if (start.value === target) {
+//         return true;
+//       }
+//     }
+//     return false;
+//   };
+
+//   return list;
+// };
+
+// var Node = function (value) {
+//   var node = {};
+
+//   node.value = value;
+//   node.next = null;
+
+//   return node;
+};
