0737f5c98523869cd45e3a28e2a0c1a6480e31fd
Mark Perry <mark.j.perry11@gmail.com>
2017-12-02 10:25:02 -0800

    Add rebalancing features

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -23,14 +21,47 @@ HashTable.prototype.insert = function(k, v) {
   if (!dupes) {
     this._storage.get(index).push([k, v]);
   }
+  this.rebalance();
 };
+
+HashTable.prototype.rebalance = function () {
+  var count = 0; 
+  var items = [];
+
+  for (var i = 0; i < this._limit; i++) {
+    if(this._storage.get(i)) {
+      count++;
+    }
+  }
+
+  if (count / this._limit >= .75) {
+    for (var bucket = 0; bucket < this._limit; bucket++) {
+      items.concat(this._storage.get(bucket));
+    }
+    this._limit = this._limit * 2;
+    this._storage = LimitedArray(this._limit);
+    for (var j = 0; j < items; j++) {
+      this.insert(items[j]);
+    }
+  } else if (count / this._limit < .25 && this._limit > 8) {
+    for (var bucket = 0; bucket < this._limit; bucket++) {
+      items.concat(this._storage.get(bucket));
+    }
+
+    this._limit = this._limit / 2;
+    this._storage = LimitedArray(this._limit);
+
+    for (var h = 0; h < items; h++) {
+      this.insert(items[h]);
+    }
+  }
+}
 
 HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   if(this._storage.get(index)) {
     for (var i = 0; i < this._storage.get(index).length; i++)  {
       if (this._storage.get(index)[i][0] === k) {
-        // this._storage.get(index)[1] = v;
         return this._storage.get(index)[i][1];
       }
     }
@@ -46,6 +77,8 @@ HashTable.prototype.remove = function(k) {
   if (this._storage.get(index)) {
     this._storage.set(index, null); 
   }
+
+  this.rebalance();
 };
 
 
